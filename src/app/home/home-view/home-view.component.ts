9c5d77581c9a04334f06a782d3c0dfa80f996a8e
pastelnata <ritabb04.05@gmail.com>
2024-11-01 16:57:19 +0100

    Fix analytics section and add default profile (incomplete)

--- src/app/home/home-view/home-view.component.ts
+++ src/app/home/home-view/home-view.component.ts
@@ -8,13 +8,22 @@ import { Profile } from '../../models/profile.model';
   styleUrl: './home-view.component.css'
 })
 export class HomeViewComponent {
+<<<<<<< HEAD
   deskHeight: number = 0;
   height: number = 50;
+=======
+  height: number = 68;
+>>>>>>> f0e9fa22 (Fix analytics section and add default profile (incomplete))
   profileTitle: string = '';
   hours: number = 0;
   minutes: number = 0;
+  defaultHeight: number = 0;
+  defaultHours: number = 0;
+  defaultMinutes: number = 0;
 
-  profiles: Profile[] = [];
+  profiles: Profile[] = [
+    { title: 'Default Profile', height: this.defaultHeight, time: `${this.defaultHours}h ${this.defaultMinutes}m` },
+  ];
 
   validateHours() {
     if (this.hours > 23) {
